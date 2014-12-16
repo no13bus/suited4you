b32334fccedc722e829436bac053bd93bed818bf
jqh <no13bus@gmail.com>
2014-12-16 09:02:52 +0800

    this is how we generate our shit.

--- lib1/reddit.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-
-
-__version__ = '0.1'
-
-class Reddit(object):
-    """docstring for Reddit"""
-    def __init__(self, arg):
-        
--- lib1/stackoverflow.py
+++
@@ -1,3 +0,0 @@
-GET /repos/:owner/:repo
-
-watch数量 fork数量 star数量 创建时间  issue 数量
